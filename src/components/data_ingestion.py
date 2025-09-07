afef25682fe46c721216a4dbfd33b20db96078d0
Dadaranger <akatoshleiwu@gmail.com>
2025-09-07 10:30:00 -0400

    model trainer completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
-#from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -98,8 +98,8 @@ if __name__ == "__main__":
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
     # Train and evaluate the model using the transformed data
-    #modeltrainer = ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
 
 
